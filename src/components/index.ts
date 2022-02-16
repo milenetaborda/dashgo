df4b95d1523a3b6e851d727ac2befc54cca65971
Milene Taborda <milenetaborda1@outlook.com>
2022-02-16 14:28:53 -0300

    chore: initial commit

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Input from './Form/Input'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import Pagination from './Pagination'
-
-export { Input, Header, Sidebar, Pagination }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,7 @@
 import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
