74fc59bc42b9217d9a8b49579e990ffd017b4ca5
Milene Taborda <milenetaborda1@outlook.com>
2022-03-30 14:07:31 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,27 @@
+<<<<<<< Updated upstream
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+=======
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
+
+import { Logo } from "./Logo";
+import { NotificationsNav } from "./NavigationsNav";
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
+>>>>>>> Stashed changes
 
 export default function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
 
+<<<<<<< Updated upstream
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
@@ -15,5 +30,14 @@ export default function Header() {
         <SignInButton />
       </div>
     </header>
+=======
+      {isWideVersion && <SearchBox />}
+
+      <Flex align="center" ml="auto">
+        <NotificationsNav />
+        <Profile showProfileData={isWideVersion} />
+      </Flex>
+    </Flex>
+>>>>>>> Stashed changes
   );
 }
